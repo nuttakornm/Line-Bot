1ef64c8df76b726399419ac725da4f6451b21bc4
nuttakornm <31401894+nuttakornm@users.noreply.github.com>
2017-08-31 11:05:48 +0700

    Update Pruksabot.php

--- Pruksabot.php
+++ Pruksabot.php
@@ -1,19 +1,7 @@
 <?php
-/**
- * Copyright 2016 LINE Corporation
- *
- * LINE Corporation licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
 
 require_once('./LINEBotTiny.php');
 $channelAccessToken = 'UJBYom3pGsI+XFblW/a3GqcL3Kah7U+t5ENQNdMKCIyB/u91jhfIHbfBkIHsRaP9Y3XFLE7M8+hPCeH0hGzI8lMMGuayWABGfUTZ4tDFTPWMQqxKNx/GNYKSF95lBGcq6T2Npz8lni5Hj3xyttHNUwdB04t89/1O/w1cDnyilFU=';
@@ -25,10 +13,9 @@ foreach ($client->parseEvents() as $event) {
             $message = $event['message'];
             switch ($message['type']) {
                 case 'text':
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'text' => $message['text']
-                    ));
+                    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+                    $response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
+                    echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
                     break;
                 default:
                     error_log("Unsupporeted message type: " . $message['type']);
