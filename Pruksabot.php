0ee862b3336fc4975702fd56421a87f96b9b4e05
nuttakornm <31401894+nuttakornm@users.noreply.github.com>
2017-08-31 11:00:47 +0700

    Update Pruksabot.php

--- Pruksabot.php
+++ Pruksabot.php
@@ -27,28 +27,7 @@ foreach ($client->parseEvents() as $event) {
                 case 'text':
                     $client->replyMessage(array(
                         'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                "type": "template",
-                                  "altText": "this is a confirm template",
-                                  "template": {
-                                      "type": "confirm",
-                                      "text": "Are you sure?",
-                                      "actions": [
-                                          {
-                                            "type": "message",
-                                            "label": "Yes",
-                                            "text": "yes"
-                                          },
-                                          {
-                                            "type": "message",
-                                            "label": "No",
-                                            "text": "no"
-                                          }
-                                      ]
-                                  }
-                            )
-                        )
+                        ''
                     ));
                     break;
                 default:
